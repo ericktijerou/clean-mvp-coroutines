b523b732e9be88e87f6b0212c67485b7c7a0e890
Erick Tijero <ericktijerou@gmail.com>
2019-09-25 17:48:16 -0500

    Add first commit

---
+++ app/src/main/java/com/ericktijerou/ericktijerorepo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ericktijerou.ericktijerorepo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
